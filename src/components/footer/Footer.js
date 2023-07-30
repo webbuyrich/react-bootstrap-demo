bd22be50912197e3c70960934566c7c88e723b64
Richard Peterson <webbuyrich@gmail.com>
2023-07-30 01:05:02 -0500

    updated components folder structure

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React  from "react";
-import { default as logo } from '../assets/brand/bootstrap-logo.svg';
+import { default as logo } from '../../assets/brand/bootstrap-logo.svg';
 
 
 const Footer = () => {
