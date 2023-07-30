60140b128c8c435bd9ccf1cde0bbdaa6259f7bc0
Richard Peterson <webbuyrich@gmail.com>
2023-07-30 01:16:17 -0500

    added NavigationLogo, NavigationLogoText

---
+++ src/components/navigation/NavigationLogoText.js
@@ -0,0 +1,10 @@
+import React  from "react";
+
+
+const NavigationLogoText = () => {
+    return(
+        <span class="fs-4 heading_text">Pricing example</span>
+    )
+}
+
+export default NavigationLogoText;
