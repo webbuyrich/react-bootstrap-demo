7b59333b3f967d00d2ff19906ab79fc110cef014
Richard Peterson <webbuyrich@gmail.com>
2023-07-30 16:13:28 -0500

    added HeaderTitle

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,11 @@
 import React  from "react";
+import HeaderTitle from "./HeaderTitle";
 
 
 const Header = () => {
     return(
         <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-            <h1 class="display-4 fw-normal text-body-emphasis">Pricing</h1>
+           <HeaderTitle />
             <p class="fs-5 text-body-secondary">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
         </div>
     )
---
+++ src/components/header/HeaderTitle.js
@@ -0,0 +1,12 @@
+import React  from "react";
+
+
+const HeaderTitle = () => {
+    return(
+        <>
+            <h1 class="display-4 fw-normal text-body-emphasis">Pricing</h1>
+        </>
+    )
+}
+
+export default HeaderTitle;
