9e3f648e26bc6b6fa0a6e3f0ccc0f9d77fe888a8
Richard Peterson <webbuyrich@gmail.com>
2023-07-30 00:47:11 -0500

    added Header.js, added class to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavigationMenu from './components/Navigation';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container py-3">
       <NavigationMenu />
+      <Header />
     </div>
   );
 }
