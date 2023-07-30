e9b33045edd030d0a5a48d31b5aee4e7bcae558d
Richard Peterson <webbuyrich@gmail.com>
2023-07-30 16:29:53 -0500

    added PriceCardFree component

--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -1,5 +1,6 @@
 import React  from "react";
 import { ReactComponent as Checkmark } from "bootstrap-icons/icons/check.svg";
+import PriceCardFree from "./PriceCardFree";
 
 
 const MainContent = () => {
@@ -7,23 +8,7 @@ const MainContent = () => {
         
         <main>
             <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-            <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm">
-                <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal">Free</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">$0<small class="text-body-secondary fw-light">/mo</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                    <li>10 users included</li>
-                    <li>2 GB of storage</li>
-                    <li>Email support</li>
-                    <li>Help center access</li>
-                    </ul>
-                    <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
-                </div>
-                </div>
-            </div>
+                <PriceCardFree />
             <div class="col">
                 <div class="card mb-4 rounded-3 shadow-sm">
                 <div class="card-header py-3">
