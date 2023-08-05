32b18e0c5518ef664d878bb5ec45e3a1ac658478
Richard Peterson <webbuyrich@gmail.com>
2023-08-04 19:56:22 -0500

    added PriceCardPro component

--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -1,6 +1,7 @@
 import React  from "react";
 import { ReactComponent as Checkmark } from "bootstrap-icons/icons/check.svg";
 import PriceCardFree from "./PriceCardFree";
+import PriceCardPro from "./PriceCardPro";
 
 
 const MainContent = () => {
@@ -9,23 +10,8 @@ const MainContent = () => {
         <main>
             <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
                 <PriceCardFree />
-            <div class="col">
-                <div class="card mb-4 rounded-3 shadow-sm">
-                <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal">Pro</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">$15<small class="text-body-secondary fw-light">/mo</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                    <li>20 users included</li>
-                    <li>10 GB of storage</li>
-                    <li>Priority email support</li>
-                    <li>Help center access</li>
-                    </ul>
-                    <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
-                </div>
-                </div>
-            </div>
+                <PriceCardPro />
+            
             <div class="col">
                 <div class="card mb-4 rounded-3 shadow-sm border-primary">
                 <div class="card-header py-3 text-bg-primary border-primary">
