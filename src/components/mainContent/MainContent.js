04a11b7a53cc566df2b0e3190351f4f0711fca20
Richard Peterson <webbuyrich@gmail.com>
2023-08-05 00:33:15 -0500

    refactored cards to PriceCardComponent.js

--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -1,17 +1,13 @@
 import React  from "react";
-import PriceCardFree from "./PriceCardFree";
-import PriceCardPro from "./PriceCardPro";
-import PriceCardEnterprise from "./PriceCardEnterprise";
 import PlansTable from "./PlansTable";
+import PriceCardComponent from "./priceCard/PriceCardComponent";
 
 const MainContent = () => {
     return(       
         
         <main>
             <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-                <PriceCardFree />
-                <PriceCardPro />            
-                <PriceCardEnterprise />
+                <PriceCardComponent />
             </div>
             <PlansTable />
             
---
+++ src/components/mainContent/priceCard/PriceCardComponent.js
@@ -0,0 +1,37 @@
+import React  from "react";
+import priceData from "./priceData.json"
+
+
+const PriceCardComponent = () => {
+    
+    //Access price card data from the JSON file
+    const cards = priceData.cards;
+    
+    return(       
+        <>
+            {cards.map((card) =>
+                <div class="col">
+                    <div class="card mb-4 rounded-3 shadow-sm">
+                        <div class="card-header py-3">
+                            <h4 class="my-0 fw-normal">{card.title}</h4>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-title pricing-card-title">${card.price}<small class="text-body-secondary fw-light">/mo</small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>{card.users}users includedss</li>
+                                <li>{card.storage} GB of storage</li>
+                                <li>{card.emailSupport} support</li>
+                                <li>{card.helpCenter}</li>
+                            </ul>
+                            {/* if statment needed to determine button bg */ }
+                            
+                            <button type="button" className={'w-100 btn btn-lg btn-primary'}>{card.callToAction}</button>
+                        </div>
+                    </div>
+                </div> 
+            )}
+        </>
+    )
+}
+
+export default PriceCardComponent;
