32ed9a860ac4077831384638e31ebcd999e77718
Richard Peterson <webbuyrich@gmail.com>
2023-08-04 20:17:40 -0500

    added PlansTable component

--- src/components/mainContent/MainContent.js
+++ src/components/mainContent/MainContent.js
@@ -1,8 +1,8 @@
 import React  from "react";
-import { ReactComponent as Checkmark } from "bootstrap-icons/icons/check.svg";
 import PriceCardFree from "./PriceCardFree";
 import PriceCardPro from "./PriceCardPro";
 import PriceCardEnterprise from "./PriceCardEnterprise";
+import PlansTable from "./PlansTable";
 
 const MainContent = () => {
     return(       
@@ -13,62 +13,8 @@ const MainContent = () => {
                 <PriceCardPro />            
                 <PriceCardEnterprise />
             </div>
-
-            <h2 class="display-6 text-center mb-4">Compare plans</h2>
-
-            <div class="table-responsive">
-            <table class="table text-center">
-                <thead>
-                <tr>
-                    <th style={{ "width": "34%"  }} ></th>
-                    <th style={{ "width": "22%"  }} >Free</th>
-                    <th style={{ "width": "22%"  }} >Pro</th>
-                    <th style={{ "width": "22%"  }} >Enterprise</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th scope="row" class="text-start">Public</th>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                </tr>
-                <tr>
-                    <th scope="row" class="text-start">Private</th>
-                    <td></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                </tr>
-                </tbody>
-
-                <tbody>
-                <tr>
-                    <th scope="row" class="text-start">Permissions</th>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                </tr>
-                <tr>
-                    <th scope="row" class="text-start">Sharing</th>
-                    <td></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                </tr>
-                <tr>
-                    <th scope="row" class="text-start">Unlimited members</th>
-                    <td></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                </tr>
-                <tr>
-                    <th scope="row" class="text-start">Extra security</th>
-                    <td></td>
-                    <td></td>
-                    <td><Checkmark className="bi" width="24" height="24" /></td>
-                </tr>
-                </tbody>
-            </table>
-            </div>
+            <PlansTable />
+            
         </main>             
     )
 }
